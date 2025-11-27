ca75cbe93b9cc88dd752fc174494d0088c6856ea
阳光下的羊驼 <lhrlhr1224@outlook.com>
2025-11-28 03:25:06 +0800

    更新仓库名

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/人生选择器/',  // GitHub Pages 部署路径
+  base: '/life_choice/',  // GitHub Pages 部署路径
   server: {
     port: 3000,
     open: true
